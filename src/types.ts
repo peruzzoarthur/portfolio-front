b3c9a4cc9f83853f697c632378f1c95a4ae6cfba
Arthur Peruzzo <peruzzoarthur@gmail.com>
2024-12-20 10:57:07 -0300

    added typescript to tags enum

--- src/types.ts
+++ src/types.ts
@@ -1,29 +1,29 @@
 export type Post = {
-  id: number
-  title: string
-  authors: Author[]
-  content: string
-  abstract: string
-  images: Image[]
-  publishedAt: string
-  createdAt: string
-  updatedAt: string
-  tags: Tag[]
+    id: number
+    title: string
+    authors: Author[]
+    content: string
+    abstract: string
+    images: Image[]
+    publishedAt: string
+    createdAt: string
+    updatedAt: string
+    tags: Tag[]
 }
 
 export type Author = {
-  id: string
-  firstName: string
-  lastName: string
+    id: string
+    firstName: string
+    lastName: string
 }
 
 export type Image = {
-  filename: string
-  id: number
-  data: {
-    type: string
-    data: number[]
-  }
+    filename: string
+    id: number
+    data: {
+        type: string
+        data: number[]
+    }
 }
 
 export enum Tag {
@@ -38,4 +38,5 @@ export enum Tag {
     TERRAFORM = 'TERRAFORM',
     TEST = 'TEST',
     TYPEORM = 'TYPEORM',
+    TYPESCRIPT = 'TYPESCRIPT',
 }
